e65bcc427353df87f0c903672347f59403fb62b4
ribo <roibeard.mac.unfraidh@gmail.com>
2019-01-12 17:25:24 +0100

    ribo0.1:Save project prior to overhaul 1

--- src/components/B.jsx
+++ src/components/B.jsx
@@ -4,6 +4,27 @@ export default function B() {
   return (
     <div>
       <h1>Hello</h1>
+
+      <div id="t"><strong>Yay.</strong> <img style="height:1.5vw;width:auto;" src="https://twemoji.maxcdn.com/2/72x72/1f44b.png"
+        alt="" />
+        ~~~~~~~~
+    <ins>1.5</ins>I'm <em><del>r.j<ins>1</ins></del>t.js</em>, a <kbd>lightweight</kbd> jQuery typewriter script.
+    I provide full <strong><u title="Hypertext Markup Language">HTML</u> tag</strong> support, also i can:<ins>1</ins>
+        <ins><span class="g">*</span> mistype (<code>&lt;kbd&gt;</code>)</ins><ins>.5</ins>
+        <ins><span class="g">*</span> delete (<code>&lt;del&gt;</code>)</ins><ins>.5</ins>
+        <ins><span class="g">*</span> delay (numeric <code>&lt;ins&gt;</code>)</ins><ins>.5</ins>
+        <ins><span class="g">*</span> insert (non-numeric <code>&lt;ins&gt;</code>)</ins><ins>.5</ins>
+
+        <a target="_blank" href="https://mntn-dev.github.io/t.js">Learn more</a>.
+
+    <ins>5</ins>(Open console:<br /><span style="border-bottom:1px dashed magenta;">$('#t').a('&lt;hr/&gt;foo&lt;hr/&gt;moo');</span>
+        )
+
+  </div>
+
+      <br />
+
+
     </div>
   )
 }
